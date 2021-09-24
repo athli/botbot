fb01c6384c490d472d6ed862a96889acdf99be95
magicat345 <78640283+magicat345@users.noreply.github.com>
2021-09-24 18:48:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,15 +17,14 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     });
 
+/*
 client.on('message', msg => {
-        if (msg.content == "derpbot, please") {
-        client.channels.cache.get('825918595904438342').send("<@!505170018556706817> 26 \n" +
-                                                       "<@!691804974719434822> 14 \n" +
-                                                       "<@!613719483051409419> 10 \n" +
-                                                       "<@!716993756343042078> 10 \n");
-        msg.reply("please say i did it");
+        if (msg.content == "derpbot test") {
+        //test code
+        msg.reply("done");
         }
 })
+*/
 
 client.on('message', msg => {
     // define args
