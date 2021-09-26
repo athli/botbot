9313c623d1de13fdff6e0288c446cbf44a8fde46
magicat345 <78640283+magicat345@users.noreply.github.com>
2021-09-25 21:04:50 -0400

    Update set_points.js

--- commands/set_points.js
+++ commands/set_points.js
@@ -72,7 +72,26 @@ module.exports = {
         if (isNaN(args[2])) {
             message.reply("leaderboard score must be the third word after !setpoints and must be a valid number");
             return;
-            
+        }
+        
+        if (score == 0) {
+            if (lb_msg.content.includes(user.id)) {
+                var leaderboard = lb_msg.content.split(' ');
+                // find user.id and index of it
+                leaderboard.forEach(element => {
+                    if (element.toString().includes(user.id)) {
+                        var name_index = leaderboard.indexOf(element);
+                        leaderboard.splice(name_index, 2);
+                        message.reply("user removed from leaderboard");
+                    }
+                }) 
+            }
+            else {
+                message.reply("user not on leaderboard");
+            }
+            return;
+        }
+        
         score = +args[2];
         if (score < 0 || !Number.isInteger(score)) {
             message.reply("only positive integers are valid scores for the leaderboard");
