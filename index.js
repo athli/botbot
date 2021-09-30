8f473bb9bb060745884882afd86b1833bd029af0
magicat345 <78640283+magicat345@users.noreply.github.com>
2021-09-30 02:37:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ client.on('ready', () => {
 //test messages
 
 client.on('message', msg => {
-        if (msg.content === "derpbot") {
-        var lb_channel = message.client.channels.cache.find(channel => channel.id === ("825918595904438342")); 
-        lb_channel.messages.fetch('891093211223838761')
-            .then(leaderboard => {
-                leaderboard.edit(leaderboard.split(' ').pop().join(' '));
-            })
-        msg.reply("done");
+        if (msg.content === "derpbot edit") {
+            var lb_channel = message.client.channels.cache.find(channel => channel.id === ("825918595904438342")); 
+            lb_channel.messages.fetch('891093211223838761')
+                .then(leaderboard => {
+                    leaderboard.edit(leaderboard.split(' ').pop().join(' '));
+                })
+            msg.reply("done");
         }
 })
 
